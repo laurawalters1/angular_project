cf506ea9d11589e88d8584e656c372e433425043
laurawalters <lauraewalters1098@gmail.com>
2022-11-08 16:16:49 +0000

    Testing git config

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   
-  title : string = 'App Title';
+  title : string = 'Test Change';
 
   constructor() { }
 
