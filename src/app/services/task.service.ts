21460e5ab2fb02952455a2bf75298cb500949f58
laurawalters <lauraewalters1098@gmail.com>
2022-11-15 11:49:00 +0000

    deleteTask method added

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http'
 import {TASKS} from '../mock-tasks'
 import {Task} from '../Task'
-import {Observable, of} from 'rxjs'
+import {Observable} from 'rxjs'
 
 @Injectable({
   providedIn: 'root'
@@ -15,5 +15,10 @@ private apiUrl = 'http://localhost:5000/tasks'
 
   getTasks(): Observable<Task[]> {
     return this.http.get<Task[]>(this.apiUrl)
+  }
+
+  deleteTask(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`
+    return this.http.delete<Task>(url)
   }
 }
