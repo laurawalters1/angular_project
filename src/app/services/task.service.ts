69ca8f9b6515e094c2e953636c8ae04742125965
laurawalters <lauraewalters1098@gmail.com>
2022-11-15 11:31:20 +0000

    Pulling tasks in from mock server

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -9,10 +9,11 @@ import {Observable, of} from 'rxjs'
 })
 export class TaskService {
 private apiUrl = 'http://localhost:5000/tasks'
-  constructor() { }
+  constructor(private http:HttpClient) {
+    
+   }
 
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS)
-    return tasks
+    return this.http.get<Task[]>(this.apiUrl)
   }
 }
